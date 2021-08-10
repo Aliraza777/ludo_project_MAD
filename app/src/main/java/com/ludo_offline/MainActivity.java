91b582abaa80c30adb3842f9a02d8a06ceb02571
Aliraza777 <Aliwahlah777@gmail.com>
2021-08-11 02:20:05 +0500

    ""

--- app/src/main/java/com/ludo_offline/MainActivity.java
+++ app/src/main/java/com/ludo_offline/MainActivity.java
@@ -8,12 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button btnplay,p2,p4;
+Button p2,p4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnplay = findViewById(R.id.enter);
         p2=findViewById(R.id.player_2);
         p4=findViewById(R.id.player_4);
 
